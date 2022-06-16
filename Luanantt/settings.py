1c2f984574b6bf6f2494fecaaffb6fe395da594c
1stmrdxs1 <75237223+1stmrdxs1@users.noreply.github.com>
2022-06-16 20:07:01 +0700

    deploy

--- Luanantt/settings.py
+++ Luanantt/settings.py
@@ -10,8 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -129,6 +130,8 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = BASE_DIR / 'static/image'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
