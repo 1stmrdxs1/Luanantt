fbc41a77d36d53b40eb916d083cbe33d59cd260c
1stmrdxs1 <75237223+1stmrdxs1@users.noreply.github.com>
2022-06-17 13:07:01 +0700

    imgv3

--- Luanantt/settings.py
+++ Luanantt/settings.py
@@ -126,13 +126,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL = 'image/'
+MEDIA_URL = 'media/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
-MEDIA_ROOT = BASE_DIR / 'static/image/'
+MEDIA_ROOT = BASE_DIR / 'static/media'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 django_heroku.settings(locals())
 
