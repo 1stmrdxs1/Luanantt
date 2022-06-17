1bfb01a0d70937dd52e0ad8bf20a3b8c2db13259
1stmrdxs1 <75237223+1stmrdxs1@users.noreply.github.com>
2022-06-17 14:37:06 +0700

    a

--- Luanantt/settings.py
+++ Luanantt/settings.py
@@ -125,20 +125,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
-
+STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
+MEDIA_ROOT = BASE_DIR / 'static/media'
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(BASE_DIR,'live-static')
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "live-static")
-
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 django_heroku.settings(locals())
 
 # Default primary key field type
